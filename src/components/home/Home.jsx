827866e2b2f91797d4d90814b4b5316ccda1946a
santichieu <santi.chieu@gmail.com>
2026-01-19 19:25:38 -0300

    Fix downloadPDF

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -3,66 +3,70 @@ import "./Home.css"
 
 const Home = () => {
   const { t, language } = useTranslate();
-  
+
 const downloadPDF = () => {
-  const isSpanish = language === "es";
 
-  const link = document.createElement("a");
+    const fileName = language === "en" 
+      ? "CV_English.pdf" 
+      : "CV.pdf"; 
 
-  link.href = isSpanish
-    ? "/Santiago Chieu Desarrollador FullStack.pdf"
-    : "/Santiago Chieu FullStack Developer english.pdf";
+    const fileUrl = `/Portfolio/${fileName}`;
 
-  link.download = isSpanish
-    ? "Santiago Chieu Desarrollador FullStack.pdf"
-    : "Santiago Chieu FullStack Developer english.pdf";
+    console.log("Ruta generada:", fileUrl);
 
-  document.body.appendChild(link);
-  link.click();
-  document.body.removeChild(link);
-};
+    const link = document.createElement("a");
+    link.href = fileUrl;
 
+    link.download = language === "en" 
+      ? "Santiago_Chieu_CV_English.pdf" 
+      : "Santiago_Chieu_CV_Espanol.pdf";
+    
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
 
   return (
     <>
-    <section id="home" className="homeContainer">
-    <h1 className="Name">Santiago Chieu</h1>
-    <div className="header">
+      <section id="home" className="homeContainer">
+        <h1 className="Name">Santiago Chieu</h1>
+        <div className="header">
           <h2>
             <span className="spantitle">
               {t("FullStack Developer")}
             </span>
           </h2>
           <div className="underlinetitle"></div>
-
         </div>
-    <ul className="social-links">
-        <li>
-          <a
-            href="https://github.com/santichieu"
-            target="_blank"
-            rel="noreferrer"
-            className="iconButton"
-          >
-            <i className="bi bi-github" style={{ fontSize: "2rem" }}></i>
-          </a>
-        </li>
-        <li>
-          <a
-            href="https://www.linkedin.com/in/santiago-chieu-171569187/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <i className="bi bi-linkedin" style={{ fontSize: "2rem" }}></i>
-          </a>
-        </li>
-      </ul>
+        
+        <ul className="social-links">
+          <li>
+            <a
+              href="https://github.com/santichieu"
+              target="_blank"
+              rel="noreferrer"
+              className="iconButton"
+            >
+              <i className="bi bi-github" style={{ fontSize: "2rem" }}></i>
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.linkedin.com/in/santiago-chieu-171569187/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <i className="bi bi-linkedin" style={{ fontSize: "2rem" }}></i>
+            </a>
+          </li>
+        </ul>
   
-      <button className="downloadbutton" onClick={downloadPDF}>{t("Download CV")}</button>
-    
-    </section>
+        <button className="downloadbutton" onClick={downloadPDF}>
+          {t("Download CV")}
+        </button>
+      </section>
     </>
   )
 }
 
-export default Home
+export default Home;
